b8d6bb1465a56d979efa56ff55f0e2b2b44ccc52
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-07-12 20:33:40 +0300

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,18 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist';
+import { configureStore } from '@reduxjs/toolkit';
+// import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+// import storage from 'redux-persist';
 import authReducer from './auth/authSlise';
-import { contactsReducer } from './contacts/contactsSlice';
+// import { contactsReducer } from './contacts/contactsSlice';
 
 export const store = configureStore({
   reducer: {
