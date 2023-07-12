8730f48d865a26431465ba6af3d12a76f66804f0
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-07-12 16:16:21 +0300

    update

---
+++ src/redux/auth/authSlise.js
@@ -0,0 +1,15 @@
+// import { createSlice } from '@reduxjs/toolkit';
+
+// const initialState = {
+//   user: { name: null, email: null },
+//   token: null,
+//   isLoggedIn: false,
+// };
+
+// const authSlice = createSlice({
+//   name: 'auth',
+//   initialState,
+//   extraReducers: {},
+// });
+
+// export default authSlice.reducer;
---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+// import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+// import storage from 'redux-persist';
+// import { authReducer } from './auth/authSlise';
+// import { contactsReducer } from './contacts/contactsSlice';
