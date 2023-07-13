c3b7b3617dbc8fe641077e4afe5e7bbcacec034a
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-07-13 07:32:34 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,8 @@ import Home from 'pages/Home/Home';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchCurrentUser } from 'redux/auth/authOperations';
-// import PrivateRoute from './PrivateRoute';
+import { RestrictedRoute } from './RestrictedRoute';
+import { PrivateRoute } from './PrivateRoute';
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -20,10 +21,32 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/registration" element={<Register />} />
-          <Route path="/logIn" element={<LogIn />} />
+          {/* <Route path="/registration" element={<Register />} /> */}
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute
+                redirectTo="/phonebook"
+                component={<Register />}
+              />
+            }
+          />
+          {/* <Route path="/logIn" element={<LogIn />} /> */}
 
-          <Route path="/phonebook" element={<Phonebook />} />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute redirectTo="/phonebook" component={<LogIn />} />
+            }
+          />
+          {/* <Route path="/phonebook" element={<Phonebook />} /> */}
+
+          <Route
+            path="/phonebook"
+            element={
+              <PrivateRoute redirectTo="/login" component={<Phonebook />} />
+            }
+          />
         </Route>
       </Routes>
     </>
